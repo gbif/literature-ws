dc7d968faab92b95ea3b569ed7a3b845be8b27bc
Mikhail Podolskiy <mike.podolskiy90@gmail.com>
2020-10-23 23:07:51 +0200

    Remove type variable from LiteratureType

--- src/main/java/org/gbif/literature/api/LiteratureType.java
+++ src/main/java/org/gbif/literature/api/LiteratureType.java
@@ -16,34 +16,24 @@
 package org.gbif.literature.api;
 
 public enum LiteratureType {
-  JOURNAL("journal"),
-  BOOK("book"),
-  GENERIC("generic"),
-  BOOK_SECTION("book_section"),
-  CONFERENCE_PROCEEDINGS("conference_proceedings"),
-  WORKING_PAPER("working_paper"),
-  REPORT("report"),
-  WEB_PAGE("web_page"),
-  THESIS("thesis"),
-  MAGAZINE_ARTICLE("magazine_article"),
-  STATUTE("statute"),
-  PATENT("patent"),
-  NEWSPAPER_ARTICLE("newspaper_article"),
-  COMPUTER_PROGRAM("computer_program"),
-  HEARING("hearing"),
-  TELEVISION_BROADCAST("television_broadcast"),
-  ENCYCLOPEDIA_ARTICLE("encyclopedia_article"),
-  CASE("case"),
-  FILM("film"),
-  BILL("bill");
-
-  private final String type;
-
-  LiteratureType(String type) {
-    this.type = type;
-  }
-
-  public String getType() {
-    return type;
-  }
+  JOURNAL,
+  BOOK,
+  GENERIC,
+  BOOK_SECTION,
+  CONFERENCE_PROCEEDINGS,
+  WORKING_PAPER,
+  REPORT,
+  WEB_PAGE,
+  THESIS,
+  MAGAZINE_ARTICLE,
+  STATUTE,
+  PATENT,
+  NEWSPAPER_ARTICLE,
+  COMPUTER_PROGRAM,
+  HEARING,
+  TELEVISION_BROADCAST,
+  ENCYCLOPEDIA_ARTICLE,
+  CASE,
+  FILM,
+  BILL;
 }
