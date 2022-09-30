ab20a7ad633d08711086ae181565efee7e2cd347
fmendezh <federicomh@gmail.com>
2022-09-30 13:25:35 +0200

    adding project identifier and programme acronym https://github.com/gbif/literature-ws/issues/8

--- src/main/java/org/gbif/literature/search/LiteratureEsFieldMapper.java
+++ src/main/java/org/gbif/literature/search/LiteratureEsFieldMapper.java
@@ -61,6 +61,8 @@ public class LiteratureEsFieldMapper implements EsFieldMapper<LiteratureSearchPa
           .put(LiteratureSearchParameter.GBIF_HIGHERTAXON_KEY, "gbifHigherTaxonKey")
           .put(LiteratureSearchParameter.CITATION_TYPE, "citationType")
           .put(LiteratureSearchParameter.GBIF_NETWORK_KEY, "gbifNetworkKey")
+          .put(LiteratureSearchParameter.GBIF_PROJECT_ID, "gbifProjectIdentifier")
+          .put(LiteratureSearchParameter.GBIF_PROGRAMME, "gbifProgrammeAcronym")
           .build();
 
   public static final Map<String, Integer> CARDINALITIES =
@@ -146,6 +148,8 @@ public class LiteratureEsFieldMapper implements EsFieldMapper<LiteratureSearchPa
       "gbifDownloadKey",
       "gbifNetworkKey",
       "peerReview",
+      "gbifProjectIdentifier",
+      "gbifProgrammeAcronym",
       "openAccess"
     };
   }
