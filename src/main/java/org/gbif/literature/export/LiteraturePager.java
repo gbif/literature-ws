5dbb4dd36a69f924664f9bb4157621ab799f5826
fmendezh <federicomh@gmail.com>
2025-01-08 10:43:48 +0100

    flushing results after each page

--- src/main/java/org/gbif/literature/export/LiteraturePager.java
+++ src/main/java/org/gbif/literature/export/LiteraturePager.java
@@ -21,25 +21,32 @@ import org.gbif.api.model.literature.search.LiteratureSearchResult;
 import org.gbif.api.util.iterables.BasePager;
 import org.gbif.literature.search.LiteratureSearchService;
 
+import java.util.function.Consumer;
+
 /** Iterates over results of {@link LiteratureSearchService#search(SearchRequest)}. */
 public class LiteraturePager extends BasePager<LiteratureSearchResult> {
 
   private final LiteratureSearchService literatureSearchService;
   private final LiteratureSearchRequest literatureSearchRequest;
+  private final Consumer<PagingResponse<LiteratureSearchResult>> onPageResponse;
 
   public LiteraturePager(
       LiteratureSearchService literatureSearchService,
       LiteratureSearchRequest literatureSearchRequest,
-      int pageSize) {
+      int pageSize,
+      Consumer<PagingResponse<LiteratureSearchResult>> onPageResponse) {
     super(pageSize);
     this.literatureSearchService = literatureSearchService;
     this.literatureSearchRequest = literatureSearchRequest;
+    this.onPageResponse = onPageResponse;
   }
 
   @Override
   public PagingResponse<LiteratureSearchResult> nextPage(PagingRequest pagingRequest) {
     literatureSearchRequest.setOffset(pagingRequest.getOffset());
     literatureSearchRequest.setLimit(pagingRequest.getLimit());
-    return literatureSearchService.exportSearch(literatureSearchRequest);
+    PagingResponse<LiteratureSearchResult> response = literatureSearchService.exportSearch(literatureSearchRequest);
+    onPageResponse.accept(response);
+    return response;
   }
 }
